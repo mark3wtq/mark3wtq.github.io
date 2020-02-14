c93244cbb4dd5e3f36332d77378a3dc2e767e1e1
mark3wtq <47373442+mark3wtq@users.noreply.github.com>
2020-02-14 12:28:28 -0600

    drawing and button size changes

--- Sites/sketch.js
+++ Sites/sketch.js
@@ -13,6 +13,15 @@ let a;
 let buttonX;
 let buttonY;
 
+let rectx;
+let recty;
+
+let sizew;
+let sizeh;
+
+
+
+
 
 
 function preload(){
@@ -20,8 +29,8 @@ function preload(){
   siteData = loadJSON("siteName.json");
 }
 
+
 function setup() {
-  // put setup code here
 
 sites = siteData.sites;
 
@@ -31,7 +40,14 @@ canvas = createCanvas(windowWidth, windowHeight);
 canvas.style("z-index", "-1");
 canvas.position(0,0);
 
-goButton()
+
+
+goButton();
+
+
+
+
+
 }
 
 function goButton(){
@@ -44,15 +60,23 @@ function goButton(){
   buttonX = random(windowWidth-5);
   buttonY = random(windowHeight-5);
 
+
+
   i = int(random(0, 24));
   names = sites[i].name;
   button = createButton(names);
+  sizew = 200;
+  sizeh = 30;
+  button.size(sizew, sizeh);
   button.style('background-color', col);
   button.position(buttonX, buttonY);
   img = loadImage(sites[i].image);
 
 
+
+
   button.mouseOver(hide);
+
 }
 
 
@@ -62,9 +86,43 @@ function hide(){
 
 }
 
-function draw() {
-  // put drawing code here
-  image(img, random(windowWidth-5), random(windowHeight-5), img.width / random(1,5), img.height / random(1,5));
+
+
+function mouseMoved(){
+  image(img, mouseX*random(1,5), mouseY*random(1,5), img.width / random(1,5), img.height / random(1,5));
+
+  sizew = sizew + 1;
+  sizeh = sizeh + 1;
+  buttonX = buttonX - 1;
+  buttonY = buttonY - 1;
+
+  if (buttonX <= 0){
+    buttonX = buttonX +1;
+    sizew = sizew - 1;
+
+  }
+  if (buttonY <= 0){
+    buttonY = buttonY +1;
+    sizeh = sizeh - 1;
+  }
+  button.size(sizew, sizeh);
+  button.position(buttonX, buttonY);
+
+
+
 
 
 }
+
+
+
+function draw() {
+  fill('rgb(100%,0%,10%)');
+  noStroke();
+  circle(rectx, recty, 66);
+
+  rectx = mouseX;
+  recty = mouseY;
+
+
+  }
